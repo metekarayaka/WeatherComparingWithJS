26f8f1b82e83571bd31abf091c924c2f9283f097
metekarayaka <42035624+metekarayaka@users.noreply.github.com>
2020-06-21 19:55:40 +0300

    Update weatherCompare.js

--- weatherCompare.js
+++ weatherCompare.js
@@ -17,19 +17,19 @@ board = new five.Board(
 // Block that is executed when the microcontroller is ready.
 board.on("ready", function() {
 
-// Initialize the button
+// Initialize the button.
   button = new five.Button(2);
   board.repl.inject({
     button: button
   });
 
-// Initialize the thermometer
+// Initialize the thermometer.
   const thermometer = new five.Thermometer({
     controller: "LM35",
     pin: "A0",
   });
 
-// Check whether themormeter is working appropriately
+// Check whether themormeter is working appropriately.
   const {celsius, fahrenheit, kelvin} = thermometer;
   if (celsius > 100){
     console.log("The temperature value is too high, check the circuit for any errors!");
@@ -38,14 +38,53 @@ board.on("ready", function() {
 // If thermometer is working appropriately, button can be pressed to
 // record the current temperature value.
   button.on("down", function() {
+    
+    // Reads the current temperature value from the microcontroller.
     const {celsius, fahrenheit, kelvin} = thermometer;
     console.log("Thermometer");
     console.log("  celsius      : ", celsius);
     console.log("  fahrenheit   : ", fahrenheit);
     console.log("  kelvin       : ", kelvin);
     console.log("--------------------------------------");
-  });
 
+    // Assign site and measured temperatures to new variables.
+    let siteTemperature = getResponse(); // PROBLEM IS HERE - THIS IS EXECUTED BEFORE THE RESPONSE IS RECEIVED - RESPONSE MUST BE RECEIVED
+    let measuredTemperature = kelvin;
   
+    // Comparing the measured and site temperatures.
+    if (measuredTemperature > parseInt(siteTemperature)){
+        console.log("Measured Temp: " + measuredTemperature + "\n" + "Site Temp: " + siteTemperature + "\n" + "Contradicts!");
+    }
+    else{
+      console.log("Measured Temp: " + measuredTemperature + "\n" + "Site Temp: " + siteTemperature + "\n" + "No Contradiction!");
+    }
 
+    // Write to the file using stream
+    const fs = require('fs');
+    fs.writeFile('out.txt', "Measured Temp: " + measuredTemperature + "\n" + "Site Temp: " + siteTemperature, (err) => {
+        if (err) throw (err);
+        console.log("Temperatures Saved!");
+    })
+  });
 });
+
+// The function that gets the temperature information from the website.
+async function getResponse() {
+    try {
+        const fetch = require("node-fetch");
+        const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Istanbul&appid=1e8899e5ddcb22a8e707a7e87f977e8d');
+        let message = await response.json();
+
+        // Convert the message to string from JSON.
+        let stringMessage = JSON.stringify(message);
+
+        // Find the temperature value. 
+        let location = stringMessage.search("temp");
+
+        // Separate the temperature value.
+        let cutMessage = stringMessage.slice(location + 6, location + 10);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
